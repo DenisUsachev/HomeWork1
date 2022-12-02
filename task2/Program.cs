aabc125393cf4803687ef3f047c7503da3802d1b
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-02 21:48:46 +0500

    Add task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+string num0 = Console.ReadLine() ??"";
+
+Console.WriteLine("Введите второе число");
+string num1 = Console.ReadLine() ??"";
+
+int number0 = Convert.ToInt32(num0);
+int number1 = Convert.ToInt32(num1);
+
+if (number0 > number1)
+{
+    Console.Write($"Число {number0} больше, числа {number1}");
+}
+else
+{
+    Console.Write($"Число {number1} больше, число {number0}");
+}
