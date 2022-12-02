f4e01f3038acf66d461535c9dcd8d33e5f7fecca
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-02 22:21:17 +0500

    Add task8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N");
+string nfirst = Console.ReadLine() ??"";
+
+int n = Convert.ToInt32(nfirst);
+
+for (int i = (1); i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
