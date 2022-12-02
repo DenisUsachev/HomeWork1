c5645e371895eed70719071a6305d428ad45fd93
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-02 21:58:58 +0500

    Add task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число");
+string num0 = Console.ReadLine() ??"";
+
+Console.WriteLine("Введите второе число");
+string num1 = Console.ReadLine() ??"";
+
+Console.WriteLine("Введите третье число");
+string num2 = Console.ReadLine() ??"";
+
+int number0 = Convert.ToInt32(num0);
+int number1 = Convert.ToInt32(num1);
+int number2 = Convert.ToInt32(num2);
+
+if ((number0 > number1) && (number0 > number2))
+{
+    Console.Write($"Число {number0} больше, числа {number1}и числа {number2}");
+}
+if ((number1 > number0) && (number1 > number2))
+{
+    Console.Write($"Число {number1} больше, числел {number0} и числа {number2}");
+}
+else
+{
+    Console.Write($"Число {number2} больше, числел {number0} и числа {number1}");
+}   
