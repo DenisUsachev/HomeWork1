88f55de8ea88de77a20813664c5403441e9a39af
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-02 22:03:44 +0500

    Add task6

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string num0 = Console.ReadLine() ??"";
+
+int number0 = Convert.ToInt32(num0);
+
+if (number0 % 2 == 0)
+{
+    Console.Write($"Число {number0}, чётное");
+}
+else
+{
+    Console.Write($"Число {number0}, нечётное");
+}
